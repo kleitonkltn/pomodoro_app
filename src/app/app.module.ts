0aef9b5d8c9d3e13123eb792fb132bd71dd23797
Kleiton de Souza Rocha <kleitonkltn@gmail.com>
2020-12-01 17:52:49 -0400

    add ng-bootstrap / set theme angular-material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser'
-import { NgModule } from '@angular/core'
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core'
 
 import { AppComponent } from './app.component'
-import { TimerComponent } from './components/timer/timer.component';
+import { TimerComponent } from './components/timer/timer.component'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { MatInputModule } from '@angular/material/input'
+import { MatCommonModule } from '@angular/material/core'
+import { MatButtonModule } from '@angular/material/button'
+import { MatIconModule } from '@angular/material/icon'
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap'
+
+
 
 @NgModule({
   declarations: [
@@ -12,9 +20,22 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatCommonModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule,
+    MatProgressBarModule,
+    NgbModule,
+  ],
+  entryComponents: [
+    TimerComponent
+  ],
+  exports: [
+    TimerComponent
   ],
   providers: [],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
