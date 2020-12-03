10faeaa34a2ead1b2a52e8018fd9c48b11138e2a
Kleiton de Souza Rocha <kleitonkltn@gmail.com>
2020-12-02 21:38:50 -0400

    Implementation of pomodoro and interval counters

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,8 @@ export class AppComponent {
   title = 'Pomodoro App'
   timeTitles = ['Pomodoro', 'Intervalo', 'Intervalo Longo']
   activeTimer = 'Pomodoro'
+  countPomodoro = 0
+  countInterval = 0
   pomodoroTime = 25
   intervalTime = 5
   longIntervalTime = 10
@@ -29,10 +31,20 @@ export class AppComponent {
     if (data.title === this.timeTitles[0]) {
       this.activeTimer = this.timeTitles[1]
       this.minutes = this.intervalTime
+      this.countPomodoro++
+      if (this.countPomodoro === 4) {
+        // implementation long interval
+      }
     }
     else if (data.title === this.timeTitles[1]) {
       this.activeTimer = this.timeTitles[0]
       this.minutes = this.pomodoroTime
+      this.countInterval++
     }
+  }
+
+  resetCounters () {
+    this.countPomodoro = 0
+    this.countInterval = 0
   }
 }
